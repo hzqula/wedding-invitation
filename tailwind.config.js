5d5550c8dbc1a80015af8d093f3ee535c59eb2ff
hzqula <ilooqstrasi@gmail.com>
2024-08-10 07:57:53 +0700

    ubah path

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,10 +32,10 @@ export default {
       },
 
       backgroundImage: {
-        inviting: "url(./assets/inviting.png)",
-        invitingPotrait: "url(./assets/inviting-potrait.jpg)",
-        hero: "url(./assets/hero.png)",
-        heroPotrait: "url(./assets/hero-potrait.jpg)",
+        inviting: "url(inviting.png)",
+        invitingPotrait: "url(inviting-potrait.jpg)",
+        hero: "url(hero.png)",
+        heroPotrait: "url(hero-potrait.jpg)",
       },
 
       screens: {
